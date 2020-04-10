00867c5efee4d14d6d9af4f50ced4a25d0bca1ac
Aaron Johnson <aaron@bigmooninc.com>
2020-04-10 12:50:26 -0500

    add local storage

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,13 @@
 <script>
   import Tailwind from "./Tailwind.svelte";
 
-  let p1Total = 0;
-  let p2Total = 0;
+  import { p1Total, p2Total } from "store";
+
+  p1Total.useLocalStorage();
+  p2Total.useLocalStorage();
+
+  // let p1Total = 0;
+  // let p2Total = 0;
   let p1HandScore = null;
   let p2HandScore = null;
   let p1ScoreInput;
@@ -51,35 +56,74 @@
   </h1>
   <div class="container w-full max-w-2xl mx-auto flex flex-row">
     <div class="flex-1 mx-3">
-      <p class="font-sans font-light uppercase text-white text-center text-lg mb-3">
+      <p
+        class="font-sans font-light uppercase text-white text-center text-lg
+        mb-3">
         Aiden
       </p>
-			<form on:submit|preventDefault={handleAddP1Score}>
-				<input bind:this={p1ScoreInput} bind:value={p1HandScore} type="number" placeholder="Aiden's Score" class="text-center text-xl w-full block bg-gray-900 text-white rounded p-3 font-sans font-thin" />
-			</form>
-			<p class="text-center font-sans font-light text-white opacity-75 my-3 text-base">Hands Played: {p1Hand}</p>
-			<div class="bg-gray-900 flex flex-col justify-center items-center rounded p-5">
-<p class="text-center font-sans font-light text-white text-lg opacity-25">Score</p>
-<p class="text-center font-sans font-bold text-white text-6xl">{p1TotalScore}</p>
-			</div>
+      <form on:submit|preventDefault={handleAddP1Score}>
+        <input
+          bind:this={p1ScoreInput}
+          bind:value={p1HandScore}
+          type="number"
+          placeholder="Aiden's Score"
+          class="text-center text-xl w-full block bg-gray-900 text-white rounded
+          p-3 font-sans font-thin" />
+      </form>
+      <p
+        class="text-center font-sans font-light text-white opacity-75 my-3
+        text-base">
+        Hands Played: {p1Hand}
+      </p>
+      <div
+        class="bg-gray-900 flex flex-col justify-center items-center rounded p-5">
+        <p
+          class="text-center font-sans font-light text-white text-lg opacity-25">
+          Score
+        </p>
+        <p class="text-center font-sans font-bold text-white text-6xl">
+          {p1TotalScore}
+        </p>
+      </div>
     </div>
     <div class="flex-1 mx-3">
-      <p class="font-sans font-light uppercase text-white text-center text-lg mb-3">Dad</p>
-			<form on:submit|preventDefault={handleAddP2Score}>
-				<input bind:this={p2ScoreInput} bind:value={p2HandScore} type="number" placeholder="Dad's Score" class="text-center text-xl w-full block bg-gray-900 text-white rounded p-3 font-sans font-thin" />
-			</form>
-			<p class="text-center font-sans font-light text-white opacity-75 my-3 text-base">Hands Played: {p2Hand}</p>
-			<div class="bg-gray-900 flex flex-col justify-center items-center rounded p-5">
-<p class="text-center font-sans font-light text-white text-lg opacity-25">Score</p>
-<p class="text-center font-sans font-bold text-white text-6xl">{p2TotalScore}</p>
-			</div>
-			
-			
-			
+      <p
+        class="font-sans font-light uppercase text-white text-center text-lg
+        mb-3">
+        Dad
+      </p>
+      <form on:submit|preventDefault={handleAddP2Score}>
+        <input
+          bind:this={p2ScoreInput}
+          bind:value={p2HandScore}
+          type="number"
+          placeholder="Dad's Score"
+          class="text-center text-xl w-full block bg-gray-900 text-white rounded
+          p-3 font-sans font-thin" />
+      </form>
+      <p
+        class="text-center font-sans font-light text-white opacity-75 my-3
+        text-base">
+        Hands Played: {p2Hand}
+      </p>
+      <div
+        class="bg-gray-900 flex flex-col justify-center items-center rounded p-5">
+        <p
+          class="text-center font-sans font-light text-white text-lg opacity-25">
+          Score
+        </p>
+        <p class="text-center font-sans font-bold text-white text-6xl">
+          {p2TotalScore}
+        </p>
+      </div>
     </div>
-		
   </div>
-	<div class="flex justify-center items-center mt-5">
-<a href="/" class="bg-black text-white text-base font-sans font-light px-8 py-3 rounded">Reset</a>
-			</div>
+  <div class="flex justify-center items-center mt-5">
+    <a
+      href="/"
+      class="bg-black text-white text-base font-sans font-light px-8 py-3
+      rounded">
+      Reset
+    </a>
+  </div>
 </div>
---
+++ src/store.js
@@ -0,0 +1,23 @@
+import { writable } from 'svelte/store';
+
+const createWritableStore = (key, startValue) => {
+  const { subscribe, set } = writable(startValue);
+
+  return {
+    subscribe,
+    set,
+    useLocalStorage: () => {
+      const json = localStorage.getItem(key);
+      if (json) {
+        set(JSON.parse(json));
+      }
+
+      subscribe((current) => {
+        localStorage.setItem(key, JSON.stringify(current));
+      });
+    },
+  };
+};
+
+export const p1Total = createWritableStore('p1Total', 0);
+export const p2Total = createWritableStore('p2Total', 0);
